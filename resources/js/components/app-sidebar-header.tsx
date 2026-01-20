0bfc06762860c1efbc413ada3c40458ce7f30235
Amran Ibrahim <amran@angroup.my>
2026-01-20 09:27:36 +0800

    Update app-sidebar-header.tsx

--- resources/js/components/app-sidebar-header.tsx
+++ resources/js/components/app-sidebar-header.tsx
@@ -1,20 +1,12 @@
 import { Breadcrumbs } from '@/components/breadcrumbs';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { type BreadcrumbItem as BreadcrumbItemType } from '@/types';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import { Search, Mail, Bell, User } from 'lucide-react';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { usePage } from '@inertiajs/react';
-import { type SharedData } from '@/types';
 
 export function AppSidebarHeader({
     breadcrumbs = [],
 }: {
     breadcrumbs?: BreadcrumbItemType[];
 }) {
-    const page = usePage<SharedData>();
-
     return (
         <header className="flex h-16 shrink-0 items-center justify-between gap-4 border-b border-gray-200 px-6 transition-[width,height] ease-linear dark:border-gray-800 dark:bg-gray-900 md:px-4">
             <div className="flex items-center gap-3 justify-start">
